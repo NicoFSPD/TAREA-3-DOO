0b2133d67e07107df0178dc464aafb31c98a05da
NicoFSPD <linegarcina@gmail.com>
2026-05-26 15:19:25 -0400

    feat: Importar todas las clases de la lógica

---
+++ src/main/java/org/example/Fanta.java
@@ -0,0 +1,23 @@
+package org.example;
+
+/**
+ * Objeto Fanta
+ */
+class Fanta extends Bebida {
+    /**
+     * constructor de la clase Fanta
+     * * @param numSerie será el ID de la Fanta comprada
+     */
+    public Fanta(int numSerie) {
+        super(numSerie);
+    }
+
+    /**
+     * acción de consumir la Fanta
+     * * @return string "fanta"
+     */
+    @Override
+    public String consumir() {
+        return "fanta";
+    }
+}
---
+++ src/main/java/org/example/Snickers.java
@@ -0,0 +1,23 @@
+package org.example;
+
+/**
+ * Objeto Snicker
+ */
+class Snickers extends Dulce {
+    /**
+     * constructor de la clase Snickers
+     * * @param numSerie será el ID del snicker comprado
+     */
+    public Snickers(int numSerie) {  //constructor
+        super(numSerie);
+    }
+
+    /**
+     * acción de consumir un snicker
+     * * @return string "snickers"
+     */
+    @Override
+    public String consumir() {
+        return "snickers";
+    }
+}
