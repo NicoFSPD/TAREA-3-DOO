92e731641b4bc6167793dbc4a5f639f6b1409aa0
Daniel Cristobal Patricio Lopez Ramirez <lopezramirezdaniel946@gmail.com>
2026-06-01 22:45:34 -0400

    feat: Creacion y desarrollo de las clases PanelComprador y PanelExpendedor

--- src/main/java/org/example/vista/PanelComprador.java
+++ src/main/java/org/example/vista/PanelComprador.java
@@ -10,15 +10,15 @@ public class PanelComprador extends JPanel {
     private PanelPrincipal parent;
 
     public PanelComprador(Expendedor exp, PanelPrincipal parent) {
-        this.exp = exp;
-        this.parent = parent;
+        this.exp = exp; //conecta la logica de la maquina con el entorno visual del comprador
+        this.parent = parent; //guarda la referencia del panel central para poder actualizar la pantalla
     }
 
     public void manejarClick(int x, int y) {
     }
 
     @Override
-    public void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) { //metodo que dibuja la interfaz grafica del lado del comprador
         g.setColor(Color.CYAN);
         g.fillRect(600, 0, 400, 700);
     }
--- src/main/java/org/example/vista/PanelExpendedor.java
+++ src/main/java/org/example/vista/PanelExpendedor.java
@@ -9,14 +9,14 @@ public class PanelExpendedor extends JPanel {
     private Expendedor exp;
 
     public PanelExpendedor(Expendedor exp) {
-        this.exp = exp;
+        this.exp = exp; //recibe y asigna el modelo logico del expendedor para conectar la vista
     }
 
     public void manejarClick(int x, int y) {
     }
 
     @Override
-    public void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) { //metodo encargado de dibujar la representacion visual de la maquina expendedora
         g.setColor(Color.LIGHT_GRAY);
         g.fillRect(50, 50, 500, 600);
 
