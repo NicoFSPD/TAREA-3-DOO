06546f3a1f8dd8e0a8a4947ae611d50f04959048
Daniel Cristobal Patricio Lopez Ramirez <lopezramirezdaniel946@gmail.com>
2026-06-01 22:34:32 -0400

    feat: Desarrollo de la clase Ventana

--- src/main/java/org/example/vista/Ventana.java
+++ src/main/java/org/example/vista/Ventana.java
@@ -1,4 +1,14 @@
 package org.example.vista;
+import javax.swing.JFrame;
 
-public class Ventana {
+public class Ventana extends JFrame {
+    public Ventana() {
+        super("Maquina Expendedora");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(1000,700); //establece las dimensiones fijas de la ventana
+        this.setResizable(false); //bloquea la opcion de cambiar el tamano de la ventana con el mouse
+        this.add(new PanelPrincipal());
+        this.setVisible(true);
+    }
 }
+
