16951bb3c412834edb41e49f08f09eb93da35145
Daniel Cristobal Patricio Lopez Ramirez <lopezramirezdaniel946@gmail.com>
2026-06-05 15:15:24 -0400

    fix/docs: Agregacion de instancia de clase PanelPrincipal para asi implementar la interfaz en la ventana, y documentacion javadoc de la clase.

--- src/main/java/org/example/frontend/Ventana.java
+++ src/main/java/org/example/frontend/Ventana.java
@@ -1,21 +1,33 @@
 package org.example.frontend;
 
-import javax.swing.*;       //esto se agregó automaticamente cuando hice el extends JFrame
+import javax.swing.*;
 
-//desde aqui se empieza a trabajar en la interfáz gráfica de la ventana visible
+/**
+ * Clase Ventana encargada de ser el espacio en donde se maneja el PanelPrincipal,
+ * el cual es el encargado de la funcionalidad de toda la interfaz grafica.
+ * @author Eduardo Riveros
+ * @author Daniel Lopez
+ * @version 1.1, 5 de junio de 2026
+ */
+
 public class Ventana extends JFrame {
-
+    /**
+     * Metodo constructor de la clase Ventana creada apartir
+     * de la superclase JFrame y de otras configuraciones base
+     * como darle nombre a la ventana, la opcion para que se pueda
+     * cerrar el programa directamente, la integracion de la clase
+     * PanelPrincipal para el funcionamiento del programa,
+     * la resolucion, una funcion que hace que se centre en la pantalla,
+     * y tambien para que sea visible.
+     */
     public Ventana(){
         super();
-
-        //cnfiguracion base para la ventana
-        this.setTitle();
+        this.setTitle("Maquina expendedora");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(1280, 720);    //resolucion
-        this.setLocationRelativeTo(null);   //eto hace que se centre la ventana en la pantalla
-
-        this.setVisible(true);  //esto hace que sea visible la ventana
+        PanelPrincipal panelPrincipal = new PanelPrincipal();
+        this.add(panelPrincipal);
+        this.setSize(1280, 720);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
     }
-
-
 }
