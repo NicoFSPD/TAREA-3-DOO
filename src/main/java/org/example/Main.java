892304275c11f51a29761d2a0aa15195cec5cde9
Daniel Cristobal Patricio Lopez Ramirez <lopezramirezdaniel946@gmail.com>
2026-06-05 12:32:19 -0400

    feat/docs: Correcion package y import de clase Main, y documentacion de esta misma clase.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
-package org.example.modelo;
+package org.example;
 
-import org.example.vista.Ventana;
+import org.example.frontend.Ventana;
 
+/**
+ * Clase principal causante de iniciar la aplicacion
+ * @author Daniel Lopez
+ * @version 1.1, 5 de junio de 2026
+ */
 public class Main {
     public static void main(String[] args) {
         new Ventana();
